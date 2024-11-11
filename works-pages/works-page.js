de241cb585c3e6e989c2f9557dabd4a91ad3071a
taigeek.wei <weikai.chu.91@gmail.com>
2024-11-11 10:22:17 +0000

    http, not "https" when connecting to the local host for testing.

--- works-pages/works-page.js
+++ works-pages/works-page.js
@@ -27,7 +27,7 @@ document.getElementById('menu-icon').addEventListener('click', function() {
 
 // --- the commeenting sections functions ---
 const url = new URL(location.href);
-const APILINK = 'https://localhost:5000/api/v1/comments';
+const APILINK = 'http://localhost:8000/api/v1/comments';
 const main = document.getElementById("commentsSection");
 
 const div_new = document.createElement('div');
